6ea8d453366e5683c0d85285d326fea89af5b1f6
waku <wakunguma13@gmail.com>
2022-04-24 15:51:08 +0200

    "added bs4"

--- gaming-news.py
+++ gaming-news.py
@@ -1,2 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://gameranx.com/news/"
+
+page = requests.get(url)
+
+
+
+scraper = BeautifulSoup(page.content, 'html.parser')
+
+print(scraper)
+
 
 
