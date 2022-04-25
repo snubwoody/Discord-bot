d0202f2680137b1a4c1211af15637bbb424580ff
waku <wakunguma13@gmail.com>
2022-04-25 18:59:31 +0200

    deleted the modules to start afresh

--- web-scraper.js
+++ web-scraper.js
@@ -1,9 +1,1 @@
-const request = require('request-promise');
-const cheerio = require('cheerio');
 
-request('https://gameranx.com/news/')
-if(!error && response.statusCode == 200){
-    const $ = cheerio.load(html);
-}
-
-const data = $('.entries-wrap')
